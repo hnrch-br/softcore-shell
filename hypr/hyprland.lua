6f09ca2ee74494699e8188ca4bbb435ca7fab59e
hnrch-br <henriquefurlandelgado@gmail.com>
2026-05-12 18:37:12 -0300

    refresh rate

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -17,7 +17,7 @@
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 hl.monitor({
     output   = "DP-1",
-    mode     = "preferred",
+    mode     = "1920x1080@144",
     position = "auto",
     scale    = "auto",
 })
